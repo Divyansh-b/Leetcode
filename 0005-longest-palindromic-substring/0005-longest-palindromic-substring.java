e63fe7d6d919f6559580ed50d717da6f00b5ee8c
div_yaansh <93183482+Divyansh-b@users.noreply.github.com>
2025-02-10 15:23:39 +0530

    Time: 20 ms (51.89%), Space: 45.1 MB (46.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,58 +1,29 @@
-// class Solution {
-//     public String longestPalindrome(String s) {
-//         String str1 = "";
-//         String str = "";
-//         String temp = "";
-//         for (int i = 0; i < s.length()-1; i++) {
-//             for (int j = s.length(); j > i; j--) {
-//                 str1 = s.substring(i, j);
-//                 if (checkReverse(str1)) {
-//                     if (str1.compareTo(temp) > 0)
-//                         temp = str1;
-//                 }
-//             }
-//         }
-//         return temp;
-//     }
-
-//     boolean checkReverse(String s) {
-//         int i = 0;
-//         int j = s.length() - 1;
-//         while (i < j) {
-//             if (s.charAt(i) != s.charAt(j)) {
-//                 return false;
-//             }
-
-//             i++;
-//             j--;
-//         }
-//         return true;
-//     }
-// }
-
-class Solution {
+public class Solution {
     public String longestPalindrome(String s) {
-
-        String temp = "";
-
-        for (int i = 0; i < s.length(); i++) {  // Loop through each character
-            for (int j = i + 1; j <= s.length(); j++) {  // Adjusted loop to include full length
-                String str1 = s.substring(i, j);
-                if (checkReverse(str1) && str1.length() > temp.length()) { // Check palindrome & length
-                    temp = str1;
-                }
+        if (s.length() <= 1) {
+            return s;
+        }
+        String maxStr = s.substring(0, 1);
+        for (int i = 0; i < s.length() - 1; i++) {
+            String odd = expandCentre(s, i, i);
+            String even = expandCentre(s, i, i + 1);
+            if (odd.length() > maxStr.length()) {
+                maxStr = odd;
+            }
+            if (even.length() > maxStr.length()) {
+                maxStr = even;
             }
         }
-        return temp;
+        return maxStr;
     }
 
-    boolean checkReverse(String s) {
-        int i = 0, j = s.length() - 1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
+
+
+    private String expandCentre(String s, int left, int right){
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                left--;
+                right++;
+            }
+            return s.substring(left+1, right);
         }
-        return true;
-    }
 }
