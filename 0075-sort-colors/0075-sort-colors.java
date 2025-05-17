d0c565ab8b1cb6f130d7bbbb6e388f3d50a64c6c
div_yaansh <93183482+Divyansh-b@users.noreply.github.com>
2025-05-17 17:48:19 +0530

    Time: 0 ms (100%), Space: 42 MB (65.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -6,43 +6,43 @@ class Solution {
     }
 
     public void sortColors(int[] nums) {
-        // int j = 0;
-        // int i = 0;
-        // int n = nums.length-1;
-        // while(i <= n){
+        int j = 0;
+        int i = 0;
+        int n = nums.length-1;
+        while(i <= n){
             
-        //     if(nums[i] == 0){
-        //         swap(nums,i,j);
-        //         i++;
-        //         j++;
-        //     }
-        //     else if(nums[i] == 2){
-        //         swap(nums,i,n);
-        //         n--;
-        //     }
-        //     else{
+            if(nums[i] == 0){
+                swap(nums,i,j);
+                i++;
+                j++;
+            }
+            else if(nums[i] == 2){
+                swap(nums,i,n);
+                n--;
+            }
+            else{
 
-        //     i++;
-        //     }
-      //  }
-      int one = 0;
-      int zero = 0;
-      int two= 0;
-      for(int i : nums){
-            if(i == 0)  zero++;
-            else if(i==1) one++;
-            else if(i == 2) two++;
-      }
+            i++;
+            }
+       }
+    //   int one = 0;
+    //   int zero = 0;
+    //   int two= 0;
+    //   for(int i : nums){
+    //         if(i == 0)  zero++;
+    //         else if(i==1) one++;
+    //         else if(i == 2) two++;
+    //   }
      
-        int index =0;
-      for( int i =0;i<zero;i++){
-        nums[index++]=0;
-      }
-      for(int i = 0;i<one;i++){
-        nums[index++]=1;
-      }
-      for( int i =0;i<two;i++){
-        nums[index++]=2;
-      }
+    // int index =0;
+    //   for( int i =0;i<zero;i++){
+    //     nums[index++]=0;
+    //   }
+    //   for(int i = 0;i<one;i++){
+    //     nums[index++]=1;
+    //   }
+    //   for( int i =0;i<two;i++){
+    //     nums[index++]=2;
+    //   }
     }
 }
